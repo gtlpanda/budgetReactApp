1ad49b2b5dd881f94be0fa9e9dc9f27c70dd786b
Carlos <c.feliciano79@gmail.com>
2021-10-28 13:05:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {Expenses} from './components/Expenses';
 
 function App() {
+
+  const expenses = [
+    {
+      id: 1,
+      title: 'Rent',
+      amount: 700.00,
+      date: new Date(2021, 9, 27)
+    }, {
+      id: 2,
+      title: 'Car insurance',
+      amount: 300.00,
+      date: new Date(2021, 9, 27)
+    }, {
+      id: 3,
+      title: 'Groceries',
+      amount: 200.00,
+      date: new Date(2021, 9, 27)
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses}/> 
+      {/* <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}/>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}/>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}/> */}
     </div>
   );
 }
---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,30 @@
+import './ExpenseDate.css';
+
+
+export const ExpenseDate = (props) => {
+
+  const month = props
+    .date.toLocaleString('en-US', {month: 'long'});
+  const day = props
+    .date
+    .toLocaleString('en-US', {day: '2-digit'});
+  const year = props
+    .date
+    .getFullYear();
+
+  return (
+
+      <div className="expense-date">
+        <div className='expense-date__month'>
+          {/* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleString */}
+          {month}
+        </div>
+        <div className='expense-date__year'>
+          {year}
+        </div>
+        <div className='expense-date__day'>
+          {day}
+        </div>
+      </div>
+  )
+}
